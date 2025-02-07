abe3835592f85e43ca79fd0a40fbd01825c95456
Pablo Garcia <pablogarciapernas@gmail.com>
2025-02-07 10:31:31 +0100

    Added some cpu structure

--- emulador_gb/src/gb.rs
+++ emulador_gb/src/gb.rs
@@ -1,15 +1,15 @@
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct Register {
-    A,
-    B,
-    C,
-    D,
-    E,
-    H,
-    L,
-    F,
-    SP,
-    PC,
+    A: u8,
+    B: u8,
+    C: u8,
+    D: u8,
+    E: u8,
+    H: u8,
+    L: u8,
+    F: u8,
+    SP: u16,
+    PC: u16,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -43,3 +43,47 @@ impl Register {
         Register::PC = 0x0100;
     }
 }
+
+
+
+pub struct CPU {
+    registers: Register,
+}
+
+impl CPU{
+    pub fn new() -> Self {
+        CPU {
+            registers: Register::new(),
+        }
+    }
+    fn get_flag(&self, flag: Flag) -> bool {
+        self.F & get_flag_bit(flag) != 0
+    }
+    
+    fn set_flag(&mut self, flag: Flag, value: bool) {
+        if value {
+            self.F |= get_flag_bit(flag);
+        } else {
+            self.F &= !get_flag_bit(flag);
+        }
+    }
+
+    fn get_af(&self) -> u16 {
+        (self.A as u16) << 8 | self.F as u16
+    }
+
+    fn get_bc(&self) -> u16 {
+        (self.B as u16) << 8 | self.C as u16
+    }
+
+    fn get_de(&self) -> u16 {
+        (self.D as u16) << 8 | self.E as u16
+    }
+
+    fn get_hl(&self) -> u16 {
+        (self.H as u16) << 8 | self.L as u16
+    }
+
+}
+
+
--- emulador_gb/src/operations.rs
+++ emulador_gb/src/operations.rs
@@ -15,7 +15,7 @@ fn half_carry_sub(a: u8, b: u8) -> bool {
     (a & 0x0F) < (b & 0x0F)
 }
 
-pub fn add(a:u8, b:u8) -> Result {
+pub fn add(a: u8, b: u8) -> Result {
     let (value, carry) = a.overflowing_add(b);
     Result {
         value,
@@ -26,7 +26,7 @@ pub fn add(a:u8, b:u8) -> Result {
     }
 }
 
-pub fn adc(a:u8, b:u8, carry:bool) -> Result {
+pub fn adc(a: u8, b: u8, carry: bool) -> Result {
     let (value, carry1) = a.overflowing_add(b);
     let (value, carry2) = value.overflowing_add(if carry { 1 } else { 0 });
     Result {
@@ -38,7 +38,7 @@ pub fn adc(a:u8, b:u8, carry:bool) -> Result {
     }
 }
 
-pub fn sub(a:u8, b:u8) -> Result {
+pub fn sub(a: u8, b: u8) -> Result {
     let (value, carry) = a.overflowing_sub(b);
     Result {
         value,
@@ -49,7 +49,7 @@ pub fn sub(a:u8, b:u8) -> Result {
     }
 }
 
-pub fn sbc(a:u8, b:u8, carry:bool) -> Result {
+pub fn sbc(a: u8, b: u8, carry: bool) -> Result {
     let (value, carry1) = a.overflowing_sub(b);
     let (value, carry2) = value.overflowing_sub(if carry { 1 } else { 0 });
     Result {
@@ -61,7 +61,7 @@ pub fn sbc(a:u8, b:u8, carry:bool) -> Result {
     }
 }
 
-pub fn and(a:u8, b:u8) -> Result {
+pub fn and(a: u8, b: u8) -> Result {
     let value = a & b;
     Result {
         value,
@@ -72,7 +72,7 @@ pub fn and(a:u8, b:u8) -> Result {
     }
 }
 
-pub fn or(a:u8, b:u8) -> Result {
+pub fn or(a: u8, b: u8) -> Result {
     let value = a | b;
     Result {
         value,
@@ -83,7 +83,7 @@ pub fn or(a:u8, b:u8) -> Result {
     }
 }
 
-pub fn xor(a:u8, b:u8) -> Result {
+pub fn xor(a: u8, b: u8) -> Result {
     let value = a ^ b;
     Result {
         value,
@@ -94,7 +94,7 @@ pub fn xor(a:u8, b:u8) -> Result {
     }
 }
 
-pub fn cp(a:u8, b:u8) -> Result {
+pub fn cp(a: u8, b: u8) -> Result {
     let (_, carry) = a.overflowing_sub(b);
     Result {
         value: a,
